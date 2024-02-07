785965da539ce7609a6f5ce65bf3efdb91d9621a
mszost <mszo@tuta.io>
2024-02-07 11:50:29 -0600

    add calculations

--- sprint-02/dice-roller/main.js
+++ sprint-02/dice-roller/main.js
@@ -1,7 +1,65 @@
-function RollD20() {
-    let roll = Math.floor(Math.random() * 20) + 1;
+function rollD20() {
+    var roll = Math.floor(Math.random() * 20) + 1;
 
-    document.getElementById("d20").innerText = roll;
+    if (roll == 1) {
+        document.getElementById("d20-txt").style.color = "var(--red)";
+        document.getElementById("d20-img").style.border = "var(--red) solid 3px";
+    } else if (roll == 20) {
+        document.getElementById("d20-txt").style.color = "var(--green)";
+        document.getElementById("d20-img").style.border = "var(--green) solid 3px";
+    } else {
+        document.getElementById("d20-txt").style.color = "var(--txt_primary)";
+        document.getElementById("d20-img").style.border = "var(--txt_primary) solid 3px";
+        roll += calculateBonuses();
+    }
+
+    document.getElementById("d20-txt").innerText = roll;
 }
 
-window.onload = RollD20;
+
+function calculateBonuses() {
+    var totalBonus = 0;
+
+    // ability score bonus
+    radios = document.getElementsByName("abilities");
+
+    for (var i=0; i<radios.length; i++) {
+        if (radios[i].checked) {
+            var ability = radios[i].value;
+            break;
+        }
+    }
+
+    if (ability != "none") {
+        var abilityBonus = (Number(document.getElementById(ability).value)-10)/2;
+        totalBonus += abilityBonus;
+    }
+
+    // proficiency and expertise bonuses
+    var usedExpr = false;
+
+    if (document.getElementById("add-prof").checked) {
+        var profBonus = Number(document.getElementById("prof-bonus").value);
+        totalBonus += profBonus;
+    } 
+
+    if (document.getElementById("add-expr").checked) {
+        usedExpr = true;
+        totalBonus += profBonus;
+    }
+
+    // inspiration bonus
+    if (document.getElementById("add-insp").checked) {
+        var inspBonus = Math.floor(Math.random() * 4) + 1;
+    }
+
+    displaySummary(abilityBonus, profBonus, usedExpr, inspBonus);
+    return totalBonus;
+
+}
+
+function displaySummary(ability, prof, expr, insp) {
+
+}
+
+window.onload = rollD20;
